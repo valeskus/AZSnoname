7e7babf4242d570d93cafe99a042653ed09a1a93
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-04-21 15:12:33 +0300

    added registrations screens. WIP

--- src/App/StackRegistration.tsx
+++ src/App/StackRegistration.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Registration} from '../screens/Registration';
+import {Registration} from '../screens/RegistrationsScreens';
+import {EnterPhone} from '../screens/RegistrationsScreens/EnterPhone';
+import {EnterCode} from '../screens/RegistrationsScreens/EnterCode';
+import {EnterUserName} from '../screens/RegistrationsScreens/EnterUserName';
+import {EnterUserBirthday} from '../screens/RegistrationsScreens/EnterUserBirthday';
 
 export type StackRegistrationParamList = {
   Main: undefined;
@@ -12,6 +16,16 @@ export function StackRegistration(): JSX.Element {
   return (
     <RegistrationStack.Navigator>
       <RegistrationStack.Screen name="Registration" component={Registration} />
+      <RegistrationStack.Screen name="EnterPhone" component={EnterPhone} />
+      <RegistrationStack.Screen name="EnterCode" component={EnterCode} />
+      <RegistrationStack.Screen
+        name="EnterUserName"
+        component={EnterUserName}
+      />
+      <RegistrationStack.Screen
+        name="EnterUserBirthday"
+        component={EnterUserBirthday}
+      />
     </RegistrationStack.Navigator>
   );
 }
---
+++ src/screens/RegistrationsScreens/EnterUserBirthday/EnterUserBirthday.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View} from 'react-native';
+import {styles} from './styles';
+
+export function EnterUserBirthday(): JSX.Element {
+  return <View style={styles.container} />;
+}
