58863c9dad8690d5f0bacb2665a078bbc0542226
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-04-21 17:43:00 +0300

    added label to code input component

--- src/UI/CodeInputs/CodeInputs.tsx
+++ src/UI/CodeInputs/CodeInputs.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import {styles} from './styles';
 import {Input} from '../Input';
 
 export function CodeInputs(): JSX.Element {
   return (
-    <View style={styles.codeInputsGroup}>
-      <View style={styles.codeInput}>
-        <Input />
-      </View>
-      <View style={styles.codeInput}>
-        <Input />
-      </View>
-      <View style={styles.codeInput}>
-        <Input />
-      </View>
-      <View style={styles.codeInput}>
-        <Input />
+    <View>
+      <Text>Введіть код з SMS</Text>
+      <View style={styles.codeInputsGroup}>
+        <View style={styles.codeInput}>
+          <Input />
+        </View>
+        <View style={styles.codeInput}>
+          <Input />
+        </View>
+        <View style={styles.codeInput}>
+          <Input />
+        </View>
+        <View style={styles.codeInput}>
+          <Input />
+        </View>
       </View>
     </View>
   );
