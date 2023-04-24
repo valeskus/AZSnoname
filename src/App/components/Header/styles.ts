084a5724068505a956ac5db18caae19d9cca793f
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-04-24 13:48:12 +0300

    move header component to foldae.REname file

--- src/App/components/Header/styles.ts
+++ src/App/components/Header/styles.ts
@@ -1,6 +1,5 @@
 import {StyleSheet} from 'react-native';
 
-// TODO: move to folder. remove header in name. file name should be .ts
 export const styles = StyleSheet.create({
   headerContainer: {
     flexDirection: 'row',
